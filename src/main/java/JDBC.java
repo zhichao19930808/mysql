cae04b2db2c2f2e79f693d8c13a47d6a35ed508f
zhichao19930808 <zhichaoglory@163.com>
2017-05-17 01:16:01 +0800

    JDBC Java Database Connectivity Java 语言数据库连接

---
+++ src/main/java/JDBC.java
@@ -0,0 +1,24 @@
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by mingfei on 5/16/17.
+ * JDBC Java Database Connectivity Java 语言数据库连接
+ */
+public class JDBC {
+
+    public static void main(String[] args) throws SQLException {
+        // 1. 准备数据库驱动
+        new Driver();
+        // 2. 取得一次数据库连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/db_1702", "root", "glory.0808");
+        // 3. 预编译语句
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO db_1702.course VALUES (NULL, 'Front-end', 2 )");
+        // 4. 执行语句
+        preparedStatement.executeUpdate();
+    }
+}
