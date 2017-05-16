7c0f069011030dc42d65ad59e25344029d6b8df0
zhichao19930808 <zhichaoglory@163.com>
2017-05-16 09:19:31 +0800

    MySQL入门

--- sql/Day01.sql
+++ sql/Day01.sql
@@ -1,1 +1,24 @@
-SHOW DATABASES 
+# 查看当前有多少库
+SHOW DATABASES ;
+#建立一个库
+CREATE DATABASE db_1702;
+#删除一个库
+DROP DATABASE db_1702;
+#查看当前库的表
+SHOW TABLES ;
+#使用某个数据库
+USE db_1702;
+#建立一个表
+CREATE TABLE t_student (
+  sno    VARCHAR(10),
+  sname  VARCHAR(10),
+  gender CHAR(1),
+  age    INT(2)
+);
+#查询数据库某个表的内容
+SELECT *
+FROM t_student;
+#为表添加一条内容
+INSERT INTO t_student
+VALUES ('2017001', '张三', '男', '18');
+
