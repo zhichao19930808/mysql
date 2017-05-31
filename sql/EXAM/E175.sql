523c9a102b8b43abd41befc48160f7f63690306e
zhichao19930808 <zhichaoglory@163.com>
2017-05-31 11:34:52 +0800

    E175

--- sql/EXAM/E175.sql
+++ sql/EXAM/E175.sql
@@ -1,41 +1,80 @@
-SHOW DATABASES ;
--- 1.设计一个能够简单实现用户注册、登录、注销的数据库表结构---------------------------------------
-DROP DATABASE IF EXISTS E175;
-CREATE DATABASE E175;
-DROP TABLE IF EXISTS E175.zhuce;
-CREATE TABLE E175.zhuce (
-  id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'id pk',
-  email VARCHAR(255) NOT NULL COMMENT '邮箱',
-  title VARCHAR(255) NOT NULL UNIQUE COMMENT '账号',
-  password VARCHAR(255) COMMENT '密码'
-);
--- 用户注册
-INSERT INTO E175.zhuce VALUES (NULL ,'邮箱','账户','密码');
--- 用户登录
-SELECT *
-FROM E175.zhuce
-WHERE (email='输入的email'OR title='输入的账号') AND password='输入的密码';
--- 2.在题 1 的基础上，实现用户留言功能------------------------------------------------------------
-DROP TABLE IF EXISTS E175.liuyan;
-CREATE TABLE E175.liuyan (
-  id      INT AUTO_INCREMENT PRIMARY KEY
-  COMMENT 'id pk',
-  liuyan  TEXT COMMENT '留言',
-  shijian DATETIME COMMENT '留言时间',
-  yonghuID INT COMMENT '留言人id'
-);
-ALTER TABLE E175.liuyan
-  ADD CONSTRAINT
-  E175_liuyan_fk_liuyanID
-FOREIGN KEY (yonghuID)
-REFERENCES E175.zhuce (id);
-INSERT INTO E175.liuyan VALUES (NULL ,'留言内容','2017-5-27 15:49:36',1);
--- 3.在题 3 的基础上，添加管理员管理留言功能，管理员无须注册，使用 SQL 初始化
--- 查看留言功能
-SELECT z.title,l.liuyan,l.shijian
-FROM E175.liuyan l CROSS JOIN E175.zhuce z
-ON l.yonghuID=z.id;
--- 删除一条留言
-DELETE FROM E175.liuyan
-WHERE id=1;
+DROP DATABASE IF EXISTS db_user;
+CREATE DATABASE db_user;
 
+DROP TABLE IF EXISTS db_user.user;
+CREATE TABLE db_user.user (
+  id       INT                   AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID PK',
+  email    VARCHAR(191) UNIQUE
+  COMMENT '邮箱',
+  username VARCHAR(255) NOT NULL
+  COMMENT '账号',
+  password VARCHAR(255) NOT NULL
+  COMMENT '密码',
+  role     VARCHAR(255) NOT NULL DEFAULT 'user'
+  COMMENT '角色：user-用户； admin-管理员'
+)
+  COMMENT '用户表';
+
+DROP TABLE IF EXISTS db_user.message;
+CREATE TABLE db_user.message (
+  id      INT               AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID PK',
+  content TEXT     NOT NULL
+  COMMENT '内容',
+  time    DATETIME NOT NULL DEFAULT now()
+  COMMENT '时间',
+  userId  INT      NOT NULL
+  COMMENT 'FK 用户ID'
+)
+  COMMENT '留言表';
+
+ALTER TABLE db_user.message
+  ADD CONSTRAINT
+  message_fk_userId
+FOREIGN KEY (userId)
+REFERENCES db_user.user (id);
+
+-- SQL
+# 1. 用户注册
+INSERT INTO db_user.user (email, username, password) VALUE ('tester@qq.com', 'Tom', '123');
+
+SELECT *
+FROM db_user.user;
+
+# 2. 登录判断
+SELECT *
+FROM db_user.user
+WHERE email = 'tester@qq.com' AND password = '123';
+
+# 3. 发布留言
+INSERT INTO db_user.message (content, userId)
+  VALUE ('留言内容1。。。', 1);
+INSERT INTO db_user.message (content, userId)
+  VALUE ('留言内容2。。。', 1);
+
+SELECT *
+FROM db_user.message;
+
+# 4. 添加管理员
+INSERT INTO db_user.user
+  VALUE (NULL, 'admin@qq.com', 'Jerry', '123', 'admin');
+
+SELECT *
+FROM db_user.user;
+
+# 5. 查询留言列表
+SELECT
+  u.username,
+  m.content,
+  m.time
+FROM db_user.user u INNER JOIN db_user.message m
+    ON u.id = m.userId;
+
+# 6. 删除留言
+DELETE FROM db_user.message
+WHERE id = 2;
+
+/*
+<input type="hidden" name="id" value="2">
+ */
