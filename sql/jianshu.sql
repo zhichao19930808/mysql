97e76d73ccd027962f76dca6cf8c2e23775886f1
zhichao19930808 <zhichaoglory@163.com>
2017-06-03 02:29:48 +0800

    简书

--- sql/jianshu.sql
+++ sql/jianshu.sql
@@ -2,6 +2,7 @@
 DROP DATABASE IF EXISTS db_jianshu;
 CREATE DATABASE db_jianshu;
 
+# 1. 用户 user----------------
 DROP TABLE IF EXISTS db_jianshu.user;
 CREATE TABLE db_jianshu.user (
   id       INT AUTO_INCREMENT PRIMARY KEY
@@ -15,6 +16,7 @@ CREATE TABLE db_jianshu.user (
 )
   COMMENT '用户表';
 
+# 2. 文集 notebook--------------
 DROP TABLE IF EXISTS db_jianshu.notebook;
 CREATE TABLE db_jianshu.notebook (
   id     INT AUTO_INCREMENT PRIMARY KEY
@@ -24,6 +26,7 @@ CREATE TABLE db_jianshu.notebook (
 )
   COMMENT '文集表';
 
+# 3. 文章 note------------------
 DROP TABLE IF EXISTS db_jianshu.note;
 CREATE TABLE db_jianshu.note (
   id         INT      AUTO_INCREMENT PRIMARY KEY
@@ -40,6 +43,7 @@ CREATE TABLE db_jianshu.note (
 )
   COMMENT '文章表';
 
+# 4. 评论 comment--------------
 DROP TABLE IF EXISTS db_jianshu.comment;
 CREATE TABLE db_jianshu.comment (
   id        INT      AUTO_INCREMENT PRIMARY KEY
@@ -54,7 +58,7 @@ CREATE TABLE db_jianshu.comment (
 )
   COMMENT '评论表';
 
-#外键---------------------------------------------------------------------------------
+#外键----------------------------------------------------
 ALTER TABLE db_jianshu.notebook
   ADD CONSTRAINT
   notebook_fk_userId
@@ -88,13 +92,110 @@ ALTER TABLE db_jianshu.comment
 FOREIGN KEY (commentId)
 REFERENCES db_jianshu.comment (id);
 
-#添加内容----------------------------------------------------------------------
+#添加内容-------------------------------------------
 INSERT INTO db_jianshu.user VALUES (NULL, '张三', 10086, '666');
 INSERT INTO db_jianshu.user VALUES (NULL, '李四', 10086, '666');
 
 INSERT INTO db_jianshu.notebook VALUES (NULL, '张三的书集', 1);
 
-INSERT INTO db_jianshu.note(content, notebookId) VALUES ('张三的第一篇文章', 1);
+INSERT INTO db_jianshu.note (content, notebookId) VALUES ('张三的第一篇文章', 1);
 
 INSERT INTO db_jianshu.comment VALUES (NULL, '李四对张三的第一篇文章的第一条评论', now(), 1, 2, NULL);
-INSERT INTO db_jianshu.comment VALUES (NULL, '张三对李四的评论的回复', now(), NULL , 1, 1);
+INSERT INTO db_jianshu.comment VALUES (NULL, '张三对李四的评论的回复', now(), NULL, 1, 1);
+
+#添加‘专题’和‘关注’功能------------------------------------------------------------------------------
+# 5. 专题 collection
+DROP TABLE IF EXISTS db_jianshu.collection;
+CREATE TABLE db_jianshu.collection (
+  id     INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'pk id',
+  title  VARCHAR(255) COMMENT '专题名称',
+  userID INT COMMENT 'fk 用户id'
+)
+  COMMENT '专题表';
+
+# 6. 专题-文章 collection_note
+DROP TABLE IF EXISTS db_jianshu.collection_note;
+CREATE TABLE db_jianshu.collection_note(
+  collectionID INT COMMENT 'pk fk ',
+  noteID INT COMMENT 'pk fk',
+  PRIMARY KEY (collectionID,noteID)
+)COMMENT '专题-文章关联表';
+
+# 7. 关注 follow
+DROP TABLE IF EXISTS db_jianshu.follow;
+CREATE TABLE db_jianshu.follow (
+  id                   INT               AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID PK',
+  time                 DATETIME NOT NULL DEFAULT now()
+  COMMENT '关注时间',
+  userID               INT COMMENT '关注者id',
+  followedUserID       INT COMMENT '被关注者id',
+  followedNotebookID   INT COMMENT '被关注的文集',
+  followedCollectionID INT COMMENT '被关注的专题'
+)
+  COMMENT '关注表';
+#外键----------------------------------------------
+ALTER TABLE db_jianshu.collection
+    ADD CONSTRAINT
+collection_fk_userID
+FOREIGN KEY (userID)
+  REFERENCES db_jianshu.user(id);
+
+
+ALTER TABLE db_jianshu.collection_note
+  ADD CONSTRAINT
+  collection_note_fk_collectionID
+FOREIGN KEY (collectionID)
+REFERENCES db_jianshu.collection (id);
+
+ALTER TABLE db_jianshu.collection_note
+  ADD CONSTRAINT
+  collection_note_fk_noteID
+FOREIGN KEY (noteID)
+REFERENCES db_jianshu.note(id);
+
+
+ALTER TABLE db_jianshu.follow
+  ADD CONSTRAINT
+  follow_fk_userID
+FOREIGN KEY (userID)
+REFERENCES db_jianshu.user (id);
+
+ALTER TABLE db_jianshu.follow
+  ADD CONSTRAINT
+  follow_fk_followUserID
+FOREIGN KEY (followedUserID)
+REFERENCES db_jianshu.user (id);
+
+ALTER TABLE db_jianshu.follow
+  ADD CONSTRAINT
+  follow_fk_followedNotebookID
+FOREIGN KEY (followedUserID)
+REFERENCES db_jianshu.follow (id);
+
+ALTER TABLE db_jianshu.follow
+  ADD CONSTRAINT
+  follow_fk_followedCollectionID
+FOREIGN KEY (followedCollectionID)
+REFERENCES db_jianshu.collection (id);
+
+#-------------------------------------------------
+INSERT INTO db_jianshu.follow (userID, followedUserID, followedNotebookID, followedCollectionID)
+VALUES (2, 1, NULL, NULL);-- 李四关注了张三
+
+INSERT INTO db_jianshu.collection(title, userID) VALUES('mySQL专题',1);-- 张三创建了mySQL专题
+
+INSERT INTO db_jianshu.collection_note (collectionID, noteID) VALUES (1, 1);-- mySQL专题 发布了张三的文章
+
+INSERT INTO db_jianshu.follow (userID, followedUserID, followedNotebookID, followedCollectionID)
+VALUES (2, NULL, NULL, 1);-- 李四关注了mySQL专题
+
+INSERT INTO db_jianshu.follow (userID, followedUserID, followedNotebookID, followedCollectionID)
+VALUES (2, NULL, 1, NULL);-- 李四关注了张三的书集
+
+
+#------------------------------------------------------------------------------------------------------
+
+
+
